208f8fa23078263382478dd1587cfb79891c2c4e
Doctor Babu Chiripireddy <doctorbabu@Doctors-MacBook-Pro.local>
2024-09-01 21:57:56 -0400

    Sept 1-1

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    Namaste React Using JSX
-  </h1>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div classname="logo-container">
+        <img
+          className="logo"
+          src="https://pics.craiyon.com/2023-05-30/af89e757b5b44b9e9888858b98817801.webp"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <h1 className="heading"> Namaste React functional component</h1>
-  </div>
-);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
